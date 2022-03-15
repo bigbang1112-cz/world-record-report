b5c7520fed0e66530f00123f6df109375ef33ea0
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-15 12:35:31 +0100

    Add version to status

--- BigBang1112.TMWR/TmwrDiscordBotService.cs
+++ BigBang1112.TMWR/TmwrDiscordBotService.cs
@@ -10,6 +10,16 @@ public class TmwrDiscordBotService : DiscordBotService
 {
     public TmwrDiscordBotService(IServiceProvider serviceProvider) : base(serviceProvider)
     {
+        
+    }
 
+    protected override async Task ReadyAsync()
+    {
+        await base.ReadyAsync();
+
+        var botVersion = typeof(TmwrDiscordBotService).Assembly.GetName().Version;
+        var wrrlibversion = typeof(WorldRecordReportLib.Data.WrContext).Assembly.GetName().Version;
+
+        await Client.SetGameAsync($"{botVersion?.ToString() ?? "unknown version"} (WrrLib: {wrrlibversion?.ToString() ?? "unknown version"})");
     }
 }
