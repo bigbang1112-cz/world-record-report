9c9ba72528bae71f9a111e3406f6f96f4308a967
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-16 01:44:45 +0100

    Add DiscordBotAttribute to TMWR

--- BigBang1112.TMWR/TmwrDiscordBotService.cs
+++ BigBang1112.TMWR/TmwrDiscordBotService.cs
@@ -2,9 +2,11 @@
 using BigBang1112.Attributes;
 using Discord.WebSocket;
 using Discord;
+using BigBang1112.Attributes.DiscordBot;
 
 namespace BigBang1112.TMWR;
 
+[DiscordBot("e7593b6b-d8f1-4caa-b950-01a8437662d0", name: "TMWR")]
 [SecretAppsettingsPath("DiscordBots:TMWR:Secret")]
 public class TmwrDiscordBotService : DiscordBotService
 {
