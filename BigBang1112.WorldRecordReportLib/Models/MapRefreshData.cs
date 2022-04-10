5f119bb0443f5fa010fa4a31559b1c5e75dda61a
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-11 01:46:37 +0200

    Add MapRefreshData ToString

--- BigBang1112.WorldRecordReportLib/Models/MapRefreshData.cs
+++ BigBang1112.WorldRecordReportLib/Models/MapRefreshData.cs
@@ -5,4 +5,9 @@ public class MapRefreshData
     public string MapUid { get; init; } = "";
     public string Name { get; init; } = "";
     public string DeformattedName { get; init; } = "";
+
+    public override string ToString()
+    {
+        return $"{DeformattedName} ({MapUid})";
+    }
 }
