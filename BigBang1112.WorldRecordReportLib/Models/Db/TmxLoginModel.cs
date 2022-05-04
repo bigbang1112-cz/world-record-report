424feacd812d7c7fb97c34e88a71db12d4333fe2
Petr Pivoňka <petrpiv1@gmail.com>
2022-05-04 20:53:46 +0200

    Escape GetMdLink

--- BigBang1112.WorldRecordReportLib/Models/Db/TmxLoginModel.cs
+++ BigBang1112.WorldRecordReportLib/Models/Db/TmxLoginModel.cs
@@ -22,7 +22,7 @@ public class TmxLoginModel : DbModel
 
     public string GetMdLink()
     {
-        return $"[{Nickname ?? UserId.ToString()}]({GetInfoUrl()})";
+        return $"[{Nickname?.EscapeDiscord() ?? UserId.ToString()}]({GetInfoUrl()})";
     }
 
     public string GetInfoUrl()
