b8534d4d2f25fd05945259d7af7fce9a44870c23
Petr Pivoňka <petrpiv1@gmail.com>
2022-05-17 20:46:05 +0200

    Add Respond

--- BigBang1112.WorldRecordReportLib/TMWR/TmwrCommand.cs
+++ BigBang1112.WorldRecordReportLib/TMWR/TmwrCommand.cs
@@ -1,4 +1,5 @@
 ﻿using BigBang1112.DiscordBot.Models;
+using Discord;
 
 namespace BigBang1112.WorldRecordReportLib.TMWR;
 
@@ -6,5 +7,15 @@ public abstract class TmwrCommand : DiscordBotCommand
 {
     public TmwrCommand(TmwrDiscordBotService tmwrDiscordBotService) : base(tmwrDiscordBotService)
     {
+    }
+
+    protected DiscordBotMessage Respond(string? title = null, string? description = null, bool ephemeral = true)
+    {
+        var embed = new EmbedBuilder()
+            .WithTitle(title)
+            .WithDescription(description)
+            .Build();
+
+        return new DiscordBotMessage(embed, ephemeral: ephemeral);
     }
 }
