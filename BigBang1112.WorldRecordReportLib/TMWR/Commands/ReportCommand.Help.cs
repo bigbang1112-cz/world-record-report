61e8f5b718d76f23836f953a8c2c3fce61e79062
Petr Pivoňka <petrpiv1@gmail.com>
2022-05-17 21:17:49 +0200

    Add /report help

---
+++ BigBang1112.WorldRecordReportLib/TMWR/Commands/ReportCommand.Help.cs
@@ -0,0 +1,13 @@
+﻿namespace BigBang1112.WorldRecordReportLib.TMWR.Commands;
+
+public partial class ReportCommand
+{
+    [DiscordBotSubCommand("help", "Gives help related to reporting using the bot.")]
+    public class Help : TmwrCommand
+    {
+        public Help(TmwrDiscordBotService tmwrDiscordBotService) : base(tmwrDiscordBotService)
+        {
+            
+        }
+    }
+}
