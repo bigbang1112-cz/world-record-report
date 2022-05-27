d0cd0cfea31e3bfc43e8ce76ed006f08dae972b7
Petr Pivoňka <petrpiv1@gmail.com>
2022-05-27 23:03:36 +0200

    Add /identify and /history nickname concepts

---
+++ BigBang1112.WorldRecordReportLib/TMWR/Commands/HistoryCommand.Nickname.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+
+namespace BigBang1112.WorldRecordReportLib.TMWR.Commands;
+
+public partial class HistoryCommand
+{
+    [DiscordBotSubCommand("nickname")]
+    public class Nickname : IdentifyBaseCommand
+    {
+        public Nickname(TmwrDiscordBotService tmwrDiscordBotService) : base(tmwrDiscordBotService)
+        {
+            
+        }
+    }
+}
---
+++ BigBang1112.WorldRecordReportLib/TMWR/Commands/IdentifyBaseCommand.cs
@@ -0,0 +1,17 @@
+﻿using Discord;
+
+namespace BigBang1112.WorldRecordReportLib.TMWR.Commands;
+
+public abstract class IdentifyBaseCommand : TmwrCommand
+{
+    [DiscordBotCommandOption("user", ApplicationCommandOptionType.String, "Login or nickname of the user.", IsRequired = true)]
+    public string? User { get; set; }
+
+    [DiscordBotCommandOption("preferlogin", ApplicationCommandOptionType.Boolean, "If someone is named after someone else's login, use this to avoid the obstruction.")]
+    public bool PreferLogin { get; set; }
+    
+    protected IdentifyBaseCommand(TmwrDiscordBotService tmwrDiscordBotService) : base(tmwrDiscordBotService)
+    {
+        
+    }
+}
